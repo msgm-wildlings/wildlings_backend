ca895bf5f167ac565652051f099e32f38ada1e1c
Cloud <thirmoon@gmail.com>
2019-10-25 01:37:57 +0800

    test sql

--- wildlings_backend/wildlings_backend/Models/Service/CustomerService.cs
+++ wildlings_backend/wildlings_backend/Models/Service/CustomerService.cs
@@ -8,7 +8,7 @@ namespace wildlings_backend.Models.Service
 {
     public class CustomerService : ICustomerService
     {
-        private ICustomerRepo _customerRepo = new FakeCustomerRepo();
+        private ICustomerRepo _customerRepo = new CustomerRepo();
 
         public IEnumerable<Customer> GetAllCustomer()
         {
--- wildlings_backend/wildlings_backend/wildlings/wildlingsContext.cs
+++ wildlings_backend/wildlings_backend/wildlings/wildlingsContext.cs
@@ -22,7 +22,7 @@ namespace wildlings_backend.wildlings
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=asdf1234;database=wildlings");
+                optionsBuilder.UseMySQL("server=localhost;port=3307;user=root;password=asdf1234;database=wildlings");
             }
         }
 
